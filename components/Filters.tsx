b5a011cb72e8b73c971d5f2dbf3c72d380e14135
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-09-19 15:54:27 +0200

    (refactor:) get list of categories & cryptos

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,7 +1,23 @@
 
+import {categories} from '../data/categories';
+import {cryptos} from '../data/cryptos';
+
 const Filters = () => {
+
+ console.log(categories);
+ console.log(cryptos);
+
   return (
-        <div>TEST 2</div>
+        <div>
+            <div>Filtres</div>
+            <div>
+                <div>
+                    <p>Crypto Monnaie</p>
+                    {categories.map((category) => <div>{category}</div>)}
+
+                </div>
+            </div>
+        </div>
   );
 };
 
