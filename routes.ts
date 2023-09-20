971c3d67a56c7429b3ac5df4c3a5689b60def9b8
Yolène <91735106+Yawleen@users.noreply.github.com>
2023-09-20 16:59:47 +0200

    modify routes.ts file

--- routes.ts
+++ routes.ts
@@ -4,5 +4,5 @@ export const LABELS_URL: string = `${HOME_URL}/labels`;
 export const CRYPTOS_URL: string = `${HOME_URL}/cryptos`;
 export const CATEGORIES_URL: string = `${HOME_URL}/categories`;
 export const CRITERIONS_URL: string = `${HOME_URL}/criterions`;
-export const COMPANY_TYPES: string = `${HOME_URL}/companyTypes`;
-export const COMPANIES: string = `${HOME_URL}/companies`;
+export const COMPANY_TYPES_URL: string = `${HOME_URL}/companyTypes`;
+export const COMPANIES_URL: string = `${HOME_URL}/companies`;
