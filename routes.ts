3844ff8c73e4b8cc62a3f7b12b37a5871faded35
Yolène <91735106+Yawleen@users.noreply.github.com>
2023-09-20 11:00:27 +0200

    add routes.ts file

---
+++ routes.ts
@@ -0,0 +1,8 @@
+const HOME_URL: string = "http://localhost:3001";
+export const PROJECTS_URL: string = `${HOME_URL}/projects`;
+export const LABELS_URL: string = `${HOME_URL}/labels`;
+export const CRYPTOS_URL: string = `${HOME_URL}/cryptos`;
+export const CATEGORIES_URL: string = `${HOME_URL}/categories`;
+export const CRITERIONS_URL: string = `${HOME_URL}/criterions`;
+export const COMPANY_TYPES: string = `${HOME_URL}/companyTypes`;
+export const COMPANIES: string = `${HOME_URL}/companies`;
